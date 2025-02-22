42e736b008784b30ca486a3bb772b1cf01ab0b9b
MRIDULSAKLANI <saklanimridul@gmail.com>
2025-02-22 21:48:30 +0530

    add

--- components/ChatBot.js
+++ components/ChatBot.js
@@ -23,7 +23,7 @@ const ChatBot = () => {
       <div className='fixed flex items-end gap-4 flex-col bottom-9 right-7 z-40'>
         <span 
           className='bot relative z-40 bg-blue-600 h-16 w-16 rounded-full flex items-center justify-center cursor-pointer'
-          onMouseEnter={handleHover} // Trigger toast on hover
+          onclick={handleHover} 
         >
           <TbMessageChatbot className='text-white text-4xl z-50' />
         </span>
